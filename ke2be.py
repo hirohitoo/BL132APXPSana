3b7ab8d707dd512ac6ff22bb28fb6b8ed40cd5e6
hirohitoo <hirohito@slac.stanford.edu>
2023-01-08 13:41:27 -0800

    Update ke2be.py

--- ke2be.py
+++ ke2be.py
@@ -2,22 +2,25 @@
 # Python needs to be >3.5
 # This script converts .csv files with header to .tsf files without header for CasaXPS
 
-replacements = [(b'filename', b'vbedge')]
+replacements = [(b'2022', b'1000')]
 globpath = './*_SUM.csv'
 
 import os
 import glob
+import pandas as pd
 
 for filepath in glob.iglob(globpath, recursive=True):
-    with open(filepath, 'rb') as file:
-        s = file.read()
-        #s = file.readlines()[1:]
-    #s2 = s.split(b’\n’, 1)[1]   
+    # Read CSV file into DataFrame df
+    df = pd.read_csv('sample.csv', index_col=0)  
     for f, r in replacements:
         #s = s.replace(f, r)
-        #if os.path.splitext(filepath)[0] contains f:
+        print(list(df.columns)[0])
+        if f in list(df.columns)[0]:
+            df.columns[0]=df.columns[0]-r
+            
         #    convert x.val to vbedge-x.val
     dest_filepath=os.path.splitext(filepath)[0]+'_BE.csv'
+   # Write Dataframe df into CSV file
     with open(dest_filepath, "wb") as file:  
         #s = s.split(b'\n', 1)[1]
         file.write(s)
